7604ca7424a2f398e7e836fd0098175c9f574d16
Danni Sanchez <loddanny9@gmail.com>
2021-06-06 19:54:25 -0600

    nokia pages

--- js/script.js
+++ js/script.js
@@ -15,5 +15,6 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // cambia cada 3 segundos
 }
+
